f0d10fbf0be005efc45cf394d0d86ad98f2149a4
Greidy Peña <greidykp@gmail.com>
2022-07-19 09:18:22 -0500

    Creacion de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 
 export const Header = () => {
   return (
-    <div className="text-4xl">
-        Header
-    </div>
+        <h1 className="text-5xl text-center font-black w-1/2 mx-auto md:w-2/3">
+            Seguimiento Pacientes {""}
+            <span className="text-indigo-600">Veterinaria</span> 
+        </h1>
   )
 }
